03008d2a37cabd8fe2eaeae62a9122a2f82d5a57
Sophie G. Opferman <sophieopferman6@gmail.com>
2021-01-04 11:59:44 -0500

    add basic shuffle function and settings page, fix gitignore

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
-# Flask-Template
-Most of my projects use the same basic stack: Flask, Bootstrap 4, and jQuery. Flask requires a bunch of directories to be set up in a strict fashion, and has hard-to-remember syntax for including templates. Bootstrap can be loaded from a CDN, which requires me to look up the link every time. Same for jQuery. I also usually include the same favicon and footer.
-By putting this all into a template repository, I am saving myself at least two minutes of work.
+# ABF-Web-Player
+Members of Stanford's ABF Drumz have to learn parts by ear, using recordings. ABF-Web-Player makes it easier in a variety of ways.
+
+## Features
+* Play, pause, back up, jump forward
+* Control tempo (speed up or slow down the recording) and volume (independently!)
+* Choice of full band or part (snare or tom) recordings
+* Notable sections within songs are "bookmarked"
 
 ## Dependencies
 `pip install flask`
 
 ## Running
 `python3 app.py`
+
+## Updating
+* The list of songs is pulled from Google Drive -- simply update the URL at the top of `app.py`
+* The "bookmarks" are stored in a database -- manually edit the database to add new ones
+
+---
+
+# To Do
+* Style
+* Global settings page -- set cookie (move cookie methods to separate file)
+* When preferred is disabled, select another option
+* Play from Google Drive
+* Deal with recordings with () differently -- snare/tom/other? (Hell beats)
+* Add bookmarks
+* Go through file list only once -- cache
--- app.py
+++ app.py
@@ -1,15 +1,147 @@
-from flask import Flask, render_template,redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
+from os import listdir
+from os.path import isfile, join
+import random
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
+#################### Config ##########################
+RECORDINGS_PATH = "static/recordings/"
+DELIMITER = " -- "
+######################################################
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    global SONGS
+    SONGS = getSongInfo()
+    return render_template('index.html', songs=SONGS)
+
+@app.route("/play")
+def playPage():
+    currSong = getSongByName(SONGS, request.args.get("song"))
+    prevSong = getSongByNumber(SONGS, currSong.getOrder() - 1)
+    nextSong = getSongByNumber(SONGS, currSong.getOrder() + 1)
+    return render_template('play.html', song=currSong, prevSong=prevSong, nextSong=nextSong)
+
+@app.route("/settings")
+def settings():
+    return render_template('settings.html')
 
 @app.route('/favicon.ico')
 def favicon():
     return redirect(url_for('static', filename='img/favicon.ico'), code=302)
+
+
+####################### Helper Functions ################################
+def getSongInfo():
+    files = sorted([f for f in listdir(RECORDINGS_PATH) if isfile(join(RECORDINGS_PATH, f))])
+
+
+    songs = []
+    workingName = ""
+    isNew = False
+    count = 0
+
+    for f in files:
+        pieces = f.split(".")[0].split(DELIMITER)
+        name = pieces[0]
+        isNew = (name != workingName)
+        if isNew:
+            thisSong = Song(name=name, order=count)
+            count = count + 1
+        else:
+            thisSong = songs[-1]
+
+        if pieces[1] == "Band":
+            if pieces[2].split(" (")[0] == "Field Drumz":
+                thisSong.addFieldDrumz(f)
+            elif pieces[2].split(" (")[0] == "Kit":
+                thisSong.addKit(f)
+            elif pieces[2].split(" (")[0] == "No Drumz":
+                thisSong.addNoDrumz(f)
+            else:
+                print("Malformed filename (Band): " + f)
+                continue
+        elif pieces[1] == "Part":
+            if pieces[2].split(" (")[0] == "Snare":
+                thisSong.addSnarePart(f)
+            elif pieces[2].split(" (")[0] == "Tom":
+                thisSong.addTomPart(f)
+            else:
+                print("Malformed filename (Part): " + f)
+                continue
+        else:
+            print("Malformed filename: " + f)
+            continue
+
+        if isNew:
+            workingName = name
+            songs.append(thisSong)
+
+    # Shuffle songs, if applicable
+    if (request.cookies.get('shuffle') == "true"):
+        los = len(songs)
+        indices = random.sample(list(range(0, los)), los)
+        i = 0
+        for s in songs:
+            s.setOrder(indices[i])
+            i = i + 1
+
+    return songs
+
+def getSongByName(songs, name):
+    for s in songs:
+        if s.getName() == name:
+            return s
+    return None
+
+def getSongByNumber(songs, num):
+    for s in songs:
+        if s.getOrder() == num:
+            return s
+    return None
+
+########################## Song Class Definition #############################
+class Song:
+    order = -1
+    hasFieldDrumz = False
+    hasKit = False
+    hasNoDrumz = False
+    hasTomPart = False
+    hasSnarePart = False
+
+    def __init__(self, name, order):
+        self.name = name
+        self.order = order
+
+    def getName(self):
+        return self.name
+
+    def setOrder(self, num):
+        self.order = num
+
+    def getOrder(self):
+        return self.order
+
+    def addTomPart(self, path):
+        self.hasTomPart = True
+        self.pathToTomPart = RECORDINGS_PATH + path
+
+    def addSnarePart(self, path):
+        self.hasSnarePart = True
+        self.pathToSnarePart = RECORDINGS_PATH + path
+
+    def addFieldDrumz(self, path):
+        self.hasFieldDrumz = True
+        self.pathToFieldDrumz = RECORDINGS_PATH + path
+
+    def addKit(self, path):
+        self.hasKit = True
+        self.pathToKit = RECORDINGS_PATH + path
+
+    def addNoDrumz(self, path):
+        self.hasNoDrumz = True
+        self.pathToNoDrumz = RECORDINGS_PATH + path
 
 
 if __name__ == "__main__":
